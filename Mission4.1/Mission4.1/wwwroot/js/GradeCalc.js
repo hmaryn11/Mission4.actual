467936a42c5d1dbffce41768e47fb93c17e1b4c6
hmaryn@byu.edu <95374695+hmaryn11@users.noreply.github.com>
2023-02-01 13:34:50 -0700

    Add existing file

---
+++ Mission4.1/Mission4.1/wwwroot/js/GradeCalc.js
@@ -0,0 +1,57 @@
+﻿//gather info from inputs from button id
+$("#buttonCalc").click(function () {
+
+    var finalNum;
+    var finalLet;
+
+    var assign = $("#assign").val();
+    var gp = $("#gp").val();
+    var quiz = $("#quiz").val();
+    var mid = $("#mid").val();
+    var final = $("#final").val();
+    var intex = $("#intex").val();
+
+    finalNum = ((assign * .5) + (gp * .1) + (quiz * .1) + (mid * .1) + (final * .1) + (intex * .1)) * 100
+
+
+    if (finalNum >= 94) {
+        finalLet = "A"
+    }
+    else if (finalNum >= 90) {
+        finalLet = "A-"
+    }
+    else if (finalNum >= 87) {
+        finalLet = "B+"
+    }
+    else if (finalNum >= 84) {
+        finalLet = "B"
+    }
+    else if (finalNum >= 80) {
+        finalLet = "B-"
+    }
+    else if (finalNum >= 77) {
+        finalLet = "C+"
+    }
+    else if (finalNum >= 74) {
+        finalLet = "C"
+    }
+    else if (finalNum >= 70) {
+        finalLet = "C-"
+    }
+    else if (finalNum >= 67) {
+        finalLet = "D+"
+    }
+    else if (finalNum >= 64) {
+        finalLet = "D"
+    }
+    else if (finalNum >= 60) {
+        finalLet = "D-"
+    }
+    else {
+        finalLet = "E"
+    }
+
+    var text = document.createTextNode("Your grade in this class is " + finalLet + ".")
+    $('#finalGrade').appendChild(text)
+})
+
